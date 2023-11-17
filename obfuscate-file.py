b724d062c323bebd7fefb3995e8a64a5f66e1300
nighty <33353036+Maxdsdsdsd@users.noreply.github.com>
2023-11-17 21:20:18 +0300

    add biopaque to obfuscate-file.py

--- obfuscate-file.py
+++ obfuscate-file.py
@@ -15,6 +15,7 @@ def run():
     settings.outline_transformer(alphabet, length)
     # settings.renamer_transformer(alphabet, length) # do not use it
     settings.number_transformer(7, True, alphabet, length)
+    settings.biopaque_transformer(alphabet, length, False) # use safe mode if you using threadings
     settings.biopaque_transformer()
     settings.controlflow_transformer(5, alphabet, length, False)
     settings.exceptionjmp_transformer(alphabet, length)
